8d472868cef1b8a1da4a4a49c5b945f9adf926d5
Remi Emonet <remi.emonet@reverse--com.heeere>
2012-04-12 00:10:07 +0200

    updates after one try

--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:.
 # we add '.' for the gstreamer symbolik link because the ubuntu package does not provide the .so file (just a .so.0 one)
 
+MOREOPT="-Djna.nosys=true"
 
-java -cp 'lib/*' fr.prima.gsp.Launcher "$@"
+java -cp 'lib/*' $MOREOPT fr.prima.gsp.Launcher "$@"
